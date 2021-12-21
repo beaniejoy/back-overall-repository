8c588783d4577dbce9be994ef34b6f5cc976c256
beaniejoy <lhb1025@gmail.com>
2021-12-21 19:23:41 +0900

    spring RestTemplate 예시 프로젝트 초기 설정

---
+++ spring-resttemplate-basic/README.md
@@ -0,0 +1,6 @@
+# RestTemplate Basic
+
+- rest-front-api
+  send request to `back-api` using RestTemplate API
+- rest-back-api
+  - receive request from `front-api` and response to `front-api`
---
+++ spring-resttemplate-basic/rest-front-api/src/main/java/io/beaniejoy/resttemplatebasic/SpringResttemplateBasicApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.resttemplatebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResttemplateBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResttemplateBasicApplication.class, args);
+    }
+
+}
