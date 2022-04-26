699d3a7a4f46fab0041c5dd23d4502e0198671dd
beaniejoy <lhb1025@gmail.com>
2022-04-26 20:24:03 +0900

    constructor 내용 수정

--- spring-jacksonbind-test/src/main/java/io/beaniejoy/jacksonbindtest/dto/chap02_constructor/MemberConstructorDto2.java
+++ spring-jacksonbind-test/src/main/java/io/beaniejoy/jacksonbindtest/dto/chap02_constructor/MemberConstructorDto2.java
@@ -1,9 +1,6 @@
 package io.beaniejoy.jacksonbindtest.dto.chap02_constructor;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import static com.fasterxml.jackson.annotation.JsonCreator.*;
 
 // 9. 인자가 한 개인 생성자만 존재하는 경우(single argument constructor)
 public class MemberConstructorDto2 {
