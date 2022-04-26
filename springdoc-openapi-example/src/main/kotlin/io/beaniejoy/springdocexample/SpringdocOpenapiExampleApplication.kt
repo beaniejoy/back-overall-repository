79d1d98cb6d96fd061ec626ffb7b088d8ba207ce
beaniejoy <lhb1025@gmail.com>
2022-04-27 03:08:14 +0900

    SpringDoc OpenAPI 프로젝트 초기 구성

---
+++ springdoc-openapi-example/src/main/kotlin/io/beaniejoy/springdocexample/SpringdocOpenapiExampleApplication.kt
@@ -0,0 +1,11 @@
+package io.beaniejoy.springdocexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringdocOpenapiExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringdocOpenapiExampleApplication>(*args)
+}
---
+++ springdoc-openapi-example/src/test/kotlin/io/beaniejoy/springdocexample/SpringdocOpenapiExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.beaniejoy.springdocexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringdocOpenapiExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
