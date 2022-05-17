d9ea51640bcc4d7274589d34dcb2a68687d0a880
beaniejoy <lhb1025@gmail.com>
2022-05-17 17:58:11 +0900

    Spring Security 강의 초기 프로젝트 세팅

---
+++ _courses/spring-security-basic/src/main/java/io/beaniejoy/basicsecurity/SpringSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.basicsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicApplication.class, args);
+    }
+
+}
