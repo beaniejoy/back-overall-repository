6737d620aaf2840d584a54e5551eb23af384d0f9
beaniejoy <lhb1025@gmail.com>
2022-05-18 00:31:02 +0900

    section1 remember-me 기능 인증

--- _courses/spring-security-basic/document/section1.md
+++ _courses/spring-security-basic/document/section1.md
@@ -1,15 +1,15 @@
 # Section 1. 스프링 시큐리티 기본 API 및 Filter 이해
 
-## 사용자 정의 보안 기능 구현
+## :pushpin: 사용자 정의 보안 기능 구현
 - `WebSecurityConfigurerAdapter`: 스프링 시큐리티 웹 보안 기능 초기화 및 설정
     - `HttpSecurity`: 세부적인 보안 기능 설정할 수 있는 API 제공
     - SecurityConfig 설정 클래스가 adapter를 상속받아서 시큐리티 내용 설정
     - 여기서 초기화 작업 진행 후 Config 파일에 등록한 사용자정의 설정 내용 적용
 
-### Form Login 인증
-- SecurityConfig.kt 참고
+## Form Login 인증
+- `SecurityConfig.kt` 참고
 
-### Form Login 인증 필터
+## Form Login 인증 필터
 - `UsernamePasswordAuthenticationFilter`
   - 인증 관련 작업을 진행하는 filter
   - `AntPathRequestMatcher("/login")`: 인증 처리 경로가 맞는지 체크
@@ -19,7 +19,18 @@
     - 성공시 `AuthenticationManager`에게 인증성공 응답
   - `Authentication`(인증 성공 후) - User 객체, Authorities (권한 정보)
   - `SecurityContext`에 저장 -> session에도 저장
-  - `SuccessHandler`: 후속 처
+  - `SuccessHandler`: 후속 처리
 - `FilterChainProxy`
   - filter들을 관리하는 bean
   - Security를 등록하면 관련 filter들도 등록된다.
+
+## Logout 처리, LogoutFilter
+- `SecurityConfig.kt` 참고
+- 세션 무효화, 인증토큰 삭제(Authentication 객체, SecurityContext), 쿠키정보 삭제, 로그인 페이지로 redirect
+
+## Remember Me 인증
+- 세션이 만료되고 웹 브라우저가 종료된 후에도 애플리케이션이 사용자를 기억하는 기능
+- 쿠키를 사용
+- 원래는 로그인을 통해 인증을 하면 `JSESSIONID`를 통해 이후 요청에 대해서 인증을 처리해준다.
+- remember-me 활성화시 `JSESSIONID`, `remember-me` cookie도 생성
+  - 이 때는 `JESSIONID`를 지워도 `remember-me` cookie 내용을 가지고 다시 인증 처리
--- _courses/spring-security-basic/src/main/java/io/beaniejoy/basicsecurity/config/SecurityConfig.kt
+++ _courses/spring-security-basic/src/main/java/io/beaniejoy/basicsecurity/config/SecurityConfig.kt
@@ -1,13 +1,22 @@
 package io.beaniejoy.basicsecurity.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.logout.LogoutHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
+    @Autowired
+    private lateinit var userDetailsService: UserDetailsService
+
     override fun configure(http: HttpSecurity) {
         // authority 인가 정책
         http
@@ -28,7 +37,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 
             // AuthenticationSuccessHandler
             .successHandler { _, response, authentication ->
-                println("authentication ${authentication.name}")
+                println("authentication - ${authentication.name}")
                 response.sendRedirect("/")
             }
             // AuthenticationFailureHandler
@@ -37,5 +46,28 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
                 response.sendRedirect("/login")
             }
             .permitAll()
+            .and()
+
+            .logout()
+            .logoutUrl("/logout")
+            // logout process 중 등록하고 싶은 작업 적용
+            .addLogoutHandler { request, _, _ ->
+                println("session invalidate")
+                val session = request.session
+                session.invalidate()
+            }
+            // logout success 이후 후처리
+            .logoutSuccessHandler { _, response, authentication ->
+                println("logout success - ${authentication.name}")
+                response.sendRedirect("/login")
+            }
+            .deleteCookies("remember-me") // 원하는 쿠키 삭제
+            .and()
+
+            .rememberMe()
+            .rememberMeParameter("remember")
+            .tokenValiditySeconds(60 * 60) // default 14일
+            .alwaysRemember(false)  // true: remember-me를 활성화하지 않아도 항상 실행
+            .userDetailsService(userDetailsService)
     }
 }
