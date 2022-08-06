a997e7dc805f0bd9e2f384893c7d7524fd943568
beaniejoy <lhb1025@gmail.com>
2022-08-06 23:59:25 +0900

    자바 JUnit5 강의 프로젝트 초기 설정

---
+++ _courses/junit-java-test/README.md
@@ -0,0 +1,3 @@
+# JUnit5 Java Test
+
+- [더 자바, 애플리케이션을 테스트하는 다양한 방법(inflearn)](https://www.inflearn.com/course/the-java-application-test)
---
+++ _courses/junit-java-test/src/main/java/io/beaniejoy/junittest/JunitJavaTestApplication.java
@@ -0,0 +1,13 @@
+package io.beaniejoy.junittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitJavaTestApplication.class, args);
+    }
+
+}
