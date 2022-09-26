7223ee439e1c7d25e16ea7f3386cdd1b1654fb85
beaniejoy <lhb1025@gmail.com>
2022-09-27 03:42:28 +0900

    kakao sdk 적용 테스트

---
+++ kakao-api-test/kakao-web/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    // 'plugin:vue/essential', // Use this if you are using Vue.js 2.x.
+    'plugin:vue/vue3-recommended', // able to use vue-cli eslint ()
+    'eslint:recommended',  // able to use eslint rules (https://eslint.org/docs/rules/)
+  ],
+  rules: {
+    /* eslint:recommended */ 
+    'semi': ["error", "always"],
+    'indent': ["error", 2],
+    // 'no-mixed-spaces-and-tabs': "error",
+    'no-unsafe-finally': ["error"],
+    'no-return-assign': ["error"],
+    'space-before-function-paren': ["error", {
+      "anonymous": "always", 
+      "named": "never", 
+      "asyncArrow": "always"
+    }],
+    'space-before-blocks': ["error"],
+    'keyword-spacing': ["error", { before: true, after: true }],
+    'object-curly-spacing': ["error", "always"],
+    'comma-spacing': ["error", { before: false, after: true }],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    /* plugin:vue/vue3-essential */ 
+    'vue/html-closing-bracket-newline': ["error", {
+      "singleline": "never",
+      "multiline": "always"
+    }],
+    'vue/html-closing-bracket-spacing': ["error"],
+    'vue/html-end-tags': ["error"],
+    'vue/html-indent': ["error", 2],
+    'vue/html-quotes': ["error"],
+    'vue/html-self-closing': ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never"
+      }
+    }]
+  },
+};
--- kakao-api-test/kakao-web/src/views/HomeView.vue
+++ kakao-api-test/kakao-web/src/views/HomeView.vue
@@ -1,15 +1,71 @@
 <template>
-  <hello-world />
+  <v-container class="mt-9 pa-0">
+    <v-card
+      class="mx-auto"
+      :max-width="size"
+      elevation="6"
+    >
+      <v-img
+        contain
+        class="white--text align-end"
+        max-height="400"
+        src="@/assets/img/health.jpg"
+        eager
+      >
+        <v-card-title class="ml-3 mb-1">
+          <span class="courgette card-title">Let's Health</span>
+        </v-card-title>
+      </v-img>
+      <v-card-actions class="mt-5 px-10">
+        <v-img
+          class="mx-auto mb-5 cursor-pointer"
+          :max-width="size * 0.5"
+          src="@/assets/img/kakao-login.png"
+          @click="login"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '@/components/HelloWorld'
+export default {
+  name: 'HomeView',
+  computed: {
+    size() {
+      switch (this.$vuetify.breakpoint.name) {
+      case 'xs':
+        return 330;
+      case 'sm':
+        return 400;
+      case 'md':
+        return 450;
+      case 'lg':
+      case 'xl':
+        return 500;
+      default:
+        return 0;
+      }
+    }
+  },
+  created() {
 
-  export default {
-    name: 'HomeView',
+    this.initKakaoSdk();
+  },
+  methods: {
+    initKakaoSdk() {
+      const Kakao = require('@/utils/kakao.min');
 
-    components: {
-      HelloWorld,
+      if (!Kakao.isInitialized()) {
+        Kakao.init('41c041072b9bd4a13ff26cde9a7394d0');
+      }
     },
+    login() {
+      const Kakao = require('@/utils/kakao.min');
+      Kakao.Auth.authorize({
+        redirectUri: `${window.location.protocol}//${window.location.host}`
+      });
+    }
   }
+};
 </script>
